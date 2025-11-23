12cdfa53a728821918ed41c1b58bd44596a164d8
Umesh Adabala <84436237+umeshadabala@users.noreply.github.com>
2025-11-23 18:28:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -537,4 +537,4 @@ def verify_owner():
         return jsonify({"success": False, "error": "Verification Failed"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
